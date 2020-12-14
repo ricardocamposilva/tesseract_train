e93c143daa36b430519ad25154c918a2506e7a99
Ricardo Campos <campos.ricardosilva@gmail.com>
2020-12-14 16:08:43 +0000

    Adjusting line ending whitespace that was printing illegal character

--- transcript_folder.py
+++ transcript_folder.py
@@ -8,15 +8,10 @@ args = sys.argv[1:]
 for file in os.listdir(args[0]):
     if file.split('.')[len(file.split('.'))-1] == 'png':
         string = pytesseract.image_to_string(f'{args[0]}/{file}', output_type='string', lang='spa_ricardo', config="--dpi 300 --psm 7")
-        data = pytesseract.image_to_data(f'{args[0]}/{file}', lang='spa_ricardo', config="--dpi 300 --psm 7")
+        # data = pytesseract.image_to_data(f'{args[0]}/{file}', lang='spa_ricardo', config="--dpi 300 --psm 7")
 
-        print(pytesseract.get_tesseract_version())
-        print(file)
-        print(string)
-        print(data)
+        print(type(string))
 
         f = open(f'{args[0]}/{file.split(".")[0]}.txt', 'w')
-        
+        f.write(str(string).strip())
         f.close()
-
-        time.sleep(3)
